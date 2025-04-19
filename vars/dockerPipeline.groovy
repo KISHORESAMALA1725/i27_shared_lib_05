6e8163a276f48428aa36a6d9c4442503219c85a8
kishore <kishore.9686@gmail.com>
2025-04-19 16:40:14 +0530

    hny

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -48,7 +48,7 @@ def call(Map pipelineParams) {
                     }            
                     steps {
                         script{
-                            buildApp("${env.APPLICATION_NAME}")
+                            buildApp("${env.APPLICATION_NAME}").call()
                         }
                     }
                 }        
